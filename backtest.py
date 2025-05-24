53121e91a16c5c6ff6caac5f0577a27b34097d43
blackongroup <info@blackongroup.com>
2025-05-24 11:28:05 -0400

    Add backtest.py

---
+++ backtest.py
@@ -0,0 +1,7 @@
+# Placeholder backtest script
+# TODO: implement rolling backtest over historical FX data
+def run_backtest():
+    print("Run your backtest logic here")
+
+if __name__ == "__main__":
+    run_backtest()
